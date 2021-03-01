272fc2512382ba41c8e1c20f924bd78fbe3b1452
m-ice <1626954415@qq.com>
2021-03-01 15:41:19 +0800

    将进销存项目至jxc.git

---
+++ Java_300集学习/src/ice/one/TestComment.java
@@ -0,0 +1,17 @@
+package ice.one;
+
+/**
+ * @author lucky_ice
+ * 版权：****
+ * 版本：version 1.0
+ */
+public class TestComment {
+    public static void main(String args[]){
+        System.out.println("测试注释");//这是打印语句
+    }
+    /*
+    我是多行注释！
+    我是多行注释！
+    我是多行注释！
+     */
+}
